d38486828b082fb9954902b29c6fb54a426ada72
Jesse Govart <govartjessy@gmail.com>
2021-06-02 14:04:26 +0200

    mainContent class

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,10 @@
 const postcssImport = require('postcss-import');
-const prefixer = require('postcss-prefix-selector');
 
 module.exports = (ctx) => ({
   map: ctx.options.map,
   plugins: [
     postcssImport({
       path: './src',
-    }),
-    prefixer({
-      prefix: '.mainContent'
     })
   ]
 });
