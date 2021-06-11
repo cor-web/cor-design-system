477dda7066c29ec7e2a90f478824fa825ebebfa6
Jesse Govart <govartjessy@gmail.com>
2021-06-11 10:53:15 +0200

    js bundle

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,45 @@ import { terser } from 'rollup-plugin-terser';
 // rollup.config.js
 export default [
   {
+
+    // ES6 output
+
     input: 'src/all.js',
     output: {
-      file: 'dist/bundle.js',
+      file: 'dist/bundle.modern.js',
+      format: 'iife'
+    },
+    plugins: [
+      terser({
+        ecma: 2018,
+        mangle: { toplevel: true },
+        compress: {
+          module: true,
+          toplevel: true,
+          unsafe_arrows: true
+        },
+        output: { quote_style: 1 }
+      })
+    ]
+  },
+  {
+    // ES5 output
+
+    input: 'src/all.js',
+    output: {
+      file: 'dist/bundle.legacy.js',
       format: 'iife'
     },
     plugins: [
       babel({ babelHelpers: 'bundled' }),
-
+      terser({
+        ecma: 2015,
+        mangle: { toplevel: true },
+        compress: {
+          toplevel: true
+        },
+        output: { quote_style: 1 }
+      })
     ]
   },
   {
--- src/all.js
+++ src/all.js
@@ -1,1 +1,1 @@
-console.log("test");
+import './components/toc/toc.js';
