ee4d62ec71f6a5e52fd1306bc105fd6801bf78f9
Jesse Govart <govartjessy@gmail.com>
2021-06-14 14:57:23 +0200

    css nano

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,27 @@
-const postcssImport = require('postcss-import');
+const postcssPresetEnv  = require('postcss-preset-env');
+const postcssImport     = require('postcss-import');
+const postcsseasings    = require('postcss-easings');
+const importUrl         = require('postcss-import-url');
+const cssnano           = require('cssnano');
 
 module.exports = (ctx) => ({
-  map: ctx.options.map,
   plugins: [
+    importUrl(),
     postcssImport({
-      path: './src',
-    })
+      path: 'app/css',
+    }),
+    postcsseasings(),
+    postcssPresetEnv({
+      stage: 0,
+      features: {
+        'logical-properties-and-values': false, 
+        'prefers-color-scheme-query': false, 
+        'gap-properties': false,
+      }
+    }),
+    cssnano({
+      preset: 'default'
+    }),
   ]
 });
 
