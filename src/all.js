0f94f35ef1c3a55edbf7b621aa267be147476966
Jesse Govart <govartjessy@gmail.com>
2021-07-07 17:42:09 +0200

    traineeship toggle

--- src/all.js
+++ src/all.js
@@ -2,3 +2,4 @@ import './components/charts/charts.js';
 import './components/toc/toc.js';
 import './components/layouts/switcher/switcher.js';
 import './components/nav/nav.js';
+import './components/toggle/toggle.js';
