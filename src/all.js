d423d92a20e1c41574291222a1c7a75cbad7076f
Jesse Govart <govartjessy@gmail.com>
2021-07-28 09:20:36 +0200

    member page

--- src/all.js
+++ src/all.js
@@ -1,4 +1,3 @@
-import './components/charts/charts.js';
 import './components/toc/toc.js';
 import './components/layouts/switcher/switcher.js';
 import './components/nav/nav.js';
--- src/components/charts/charts.js
+++ src/components/charts/charts.js
@@ -1,5 +1,3 @@
 import './bar-graph/bar-graph';
 import './multi-bar-graph/multi-bar-graph';
 import './cor-filter/cor-filter';
-import '../tooltip/tooltip';
-import '../toc/toc.js';
