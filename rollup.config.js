a47bb9ab54a00b715b59525f6248de9b9697e886
Jessy Govart <jessie.govart@cor.europa.eu>
2021-08-31 16:46:46 +0200

    traineeships icons

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import versionInjector from 'rollup-plugin-version-injector';
+import html from '@web/rollup-plugin-html';
+
+
+
 
 
 // rollup.config.js
 export default [
+
   {
 
     // ES6 output
@@ -78,6 +83,50 @@ export default [
     input: 'src/components/charts/charts.js',
     output: {
       file: 'dist/charts/bundle.legacy.js',
+      format: 'iife'
+    },
+    plugins: [
+      babel({ babelHelpers: 'bundled' }),
+      terser({
+        ecma: 2015,
+        mangle: { toplevel: true },
+        compress: {
+          toplevel: true
+        },
+        output: { quote_style: 1 }
+      }),
+      versionInjector()
+    ]
+  },
+  {
+
+    // ES6 output
+
+    input: 'src/components/pages/jobs/jobs.js',
+    output: {
+      file: 'dist/jobs/bundle.modern.js',
+      format: 'iife'
+    },
+    plugins: [
+      terser({
+        ecma: 2018,
+        mangle: { toplevel: true },
+        compress: {
+          module: true,
+          toplevel: true,
+          unsafe_arrows: true
+        },
+        output: { quote_style: 1 }
+      }),
+      versionInjector()
+    ]
+  },
+  {
+    // ES5 output
+
+    input: 'src/components/pages/jobs/jobs.js',
+    output: {
+      file: 'dist/jobs/bundle.legacy.js',
       format: 'iife'
     },
     plugins: [
--- src/components/pages/jobs/jobs.js
+++ src/components/pages/jobs/jobs.js
@@ -1,20 +1,13 @@
+import '../../carousel/carousel.js';
 
-// Set things up
 
 const textElements = document.querySelectorAll('.cor-jobs__wordcloud li,[observe]');
 
 const observer = new IntersectionObserver(handleIntersect);
 textElements.forEach( textElement => observer.observe(textElement));
-  
-
 
 function handleIntersect(entries) {
   entries.forEach((entry) => {
-     
-    
-    console.log(entry.target);
-    console.log(entry.isIntersecting);
-    
      
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
