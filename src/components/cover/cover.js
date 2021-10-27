a4836d477142f888609167ddb387d9bd01e33edd
Pol Pasop <govartjessy@gmail.com>
2021-10-27 09:27:30 +0200

    member profile in content

---
+++ src/components/cover/cover.js
@@ -0,0 +1,21 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+    :host {
+    display: block;
+    }
+  </style>
+  <slot name="title"></slot>
+  <slot name="description"></slot>
+  <slot name="image"></slot>
+`;
+
+class CorCover extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define("cor-cover", CorCover);
