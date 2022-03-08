66d5a3bbbf0b4df222cc4db318334ec9e54c1252
Jessy Govart <jessie.govart@cor.europa.eu>
2022-03-08 09:59:25 +0100

    expanding list button

--- src/components/expandinglist/expandinglist.js
+++ src/components/expandinglist/expandinglist.js
@@ -2,13 +2,57 @@ const template = document.createElement('template');
 template.innerHTML = `
   <style>
     :host {
-    display: block;
+      display: grid;
     }
 
+    .cor-btn {
+      background-color: white;
+      border-radius: var(--button-border-radius);
+      color: var(--button-background-color);
+      display: inline-block;
+      font-weight: var(--button-font-weight);
+      text-align: center;
+      white-space: nowrap;
+      vertical-align: middle;
+      user-select: none;
+      border: var(--button-border-width, 1px) solid var(--button-background-color);
+      margin: 0 auto;
+      padding: var(--button-padding-y) var(--button-padding-x);
+    }
+
+    .cor-btn:hover {
+      color: var(--button-color-hover);
+      background-color: var(--button-background-color-hover);
+      border-color: var(--button-border-color-hover);
+    }
+    
+    .cor-btn:focus {
+      outline: 0;
+      box-shadow: var(--button-focus-box-shadow);
+      text-decoration: none;
+    }
+    
+    .cor-btn:disabled {
+      opacity: var(--button-disabled-opacity);
+      box-shadow: none;
+    }
+    
+    .cor-btn:not(:disabled):not(.disabled) {
+      cursor: pointer;
+    }
+
+    .cor-btn:not(:disabled):not(.disabled):active {
+      background-image: none;
+      box-shadow: var(--button-active-box-shadow);
+    }
+
+    .cor-btn:not(:disabled):not(.disabled):active :focus {
+      box-shadow: var(--button-focus-box-shadow) var(--button-active-box-shadow);
+    }
 
   </style>
   <slot></slot>
-  <button aria-hidden="true">Read More</button>
+  <button class="cor-btn" aria-hidden="true">Read More</button>
 `;
 
 class ExpandingList extends HTMLElement {
