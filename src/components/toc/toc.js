7cd8fc92382ffc669ac7ea8efdbd0fcb12793db7
jgova <jessie.govart@cor.europa.eu>
2022-03-22 20:08:06 +0100

    toc active state

--- src/components/toc/toc.js
+++ src/components/toc/toc.js
@@ -6,17 +6,49 @@ class TableOfContent extends HTMLElement {
     this.render();
   }
 
+  connectedCallback() {
+    var options = {
+      root: document.querySelector(".cor-container"),
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver((entries, options) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute("id");
+        if (entry.intersectionRatio > 0) {
+          console.log("ici");
+          this.shadowRoot
+            .querySelector(`nav li a[href="#${id}"]`)
+            .parentElement.classList.add("active");
+        } else {
+          console.log("la");
+          this.shadowRoot
+            .querySelector(`nav li a[href="#${id}"]`)
+            .parentElement.classList.remove("active");
+        }
+      });
+    });
+
+    // Track all sections that have an `id` applied
+    document.querySelectorAll("h2[id],h3[id]").forEach((section) => {
+      observer.observe(section);
+    });
+  }
+
   getSections() {
-    const sectionsElements = document.querySelectorAll('.cor-container h2[id],.cor-container h3[id],.cor-rich-text h2');
+    const sectionsElements = document.querySelectorAll(
+      ".cor-container h2[id],.cor-container h3[id],.cor-rich-text h2"
+    );
 
     this.sections = [...sectionsElements].map((section, i) => {
-      section.classList.add('cor-anchor');
+      section.classList.add("cor-anchor");
 
       if (!section.id) {
         section.id = i;
       }
 
-      return ({ id: section.id, title: section.textContent });
+      return { id: section.id, title: section.textContent };
     });
 
     return this.sections;
@@ -27,45 +59,51 @@ class TableOfContent extends HTMLElement {
 
     this.root.innerHTML = `
       <style>
-        ul {
-          display: grid;
-          gap: var(--space-4xs);
-          margin: var(--space-3xs) 0 0 1rem;
+        ul {  
+          margin: var(--space-3xs) 0 0 0;
           padding: 0;
+          list-style: none;
         }
         
         ul > li {
-          list-style-position: outside;
-          list-style-type: disc;
+          border-left: solid 2px var(--oc-gray-3);
+          padding: var(--space-4xs) var(--space-2xs);
           line-height: initial;
+        }
+
+        ul > li.active {
+          background-color: rgba(var(--oc-blue-8-rgb), .1);
+          border-left: solid 2px var(--oc-blue-8);
         }
         
         h2 {
           color: var(--g-8);
           font-size: var(--step--3) !important;
           font-weight: bold;
-          text-transform: initial;
+          letter-spacing: 1.5px;
+          text-transform: uppercase;
           margin: 0 0 var(--space--4xs) 0;
         }
         
         a {
           font-size: var(--step--4);
-          text-decoration: underline;
+          text-decoration: none;
           margin-top: var(--space-4xs, 1rem);
           outline: 2px solid transparent;
           outline-offset: 5px;
           transition: outline-offset 0.2s linear;
         }
         
-        a:hover {
-          text-decoration: none;
-        }
-
         a:link,
         a:focus,
         a:visited,
         a:active {
           color: var(--link-color);
+        }
+
+        a:hover {
+          text-decoration: none;
+          color: var(--txt-color);
         }
 
         a:active {
@@ -87,18 +125,22 @@ class TableOfContent extends HTMLElement {
       </style>
       <nav class="cor-toc cor-toc--sticky" aria-labelledby="sections-heading">
         <h2 id="sections-heading"><slot name="toc-title">Contents</slot></h2>
-        <ul class="list-disc">
-          ${this.sections.map(section => `
+        <ul>
+          ${this.sections
+            .map(
+              (section) => `
             <li>
               <a href="#${section.id}">${section.title}</a>
             </li>
-          `).join('')}
+          `
+            )
+            .join("")}
         </ul>
       </nav>
     `;
   }
 }
 
-if (!customElements.get('cor-toc')) {
-  customElements.define('cor-toc', TableOfContent);
+if (!customElements.get("cor-toc")) {
+  customElements.define("cor-toc", TableOfContent);
 }
