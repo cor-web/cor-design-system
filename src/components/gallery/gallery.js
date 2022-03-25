7faabe93d45c34f4ae6d8bcaa3029fddf839ed1d
jgova <jessie.govart@cor.europa.eu>
2022-03-25 17:09:52 +0100

    photo gallery

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,16 +1,28 @@
+import lightGallery from 'https://cdn.skypack.dev/lightgallery@2.0.0-beta.4';
+import lgZoom from 'https://cdn.skypack.dev/lightgallery@2.0.0-beta.4/plugins/zoom';
+import lgThumbnail from 'https://cdn.skypack.dev/lightgallery@2.0.0-beta.4/plugins/thumbnail';
 
 class CorGallery extends HTMLElement {
   constructor() {
     super();
 
-    lightGallery(document.getElementById('lightgallery'), {
-        plugins: [lgZoom, lgThumbnail],
-        speed: 500
+    const $lgContainer = document.getElementById('gallery-container');
+
+    const lg = lightGallery($lgContainer, {
+      speed: 500,
+      container: $lgContainer,
+      // Do not allow users to close the gallery
+      closable: false,
+      // Add maximize icon to enlarge the gallery
+      showMaximizeIcon: true,
+      plugins: [lgZoom, lgThumbnail],
+      licenseKey: 45,
     });
+    lg.openGallery(0);
   }
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
   if (!customElements.get('cor-gallery')) {
     customElements.define('cor-gallery', CorGallery);
   }
