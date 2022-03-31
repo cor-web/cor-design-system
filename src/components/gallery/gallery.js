f491d897f6b8211b18b729302fef5c9bfb09fa4f
Jessy Govart <jessie.govart@cor.europa.eu>
2022-03-31 13:13:36 +0200

    photo gallery buttons

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -2,7 +2,7 @@ import lightGallery from "https://cdn.skypack.dev/lightgallery@2.1.2";
 import lgZoom from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/zoom";
 import lgThumbnail from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/thumbnail";
 import lgAutoplay from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay";
-import lgFullscreen from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/fullscreen";
+
 class CorGallery extends HTMLElement {
   constructor() {
     super();
@@ -21,7 +21,6 @@ class CorGallery extends HTMLElement {
       plugins: [
         lgZoom,
         lgThumbnail,
-        lgFullscreen,
         lgAutoplay
       ],
       mobileSettings: {
