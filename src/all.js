614f580ce6ed5f6203e68d9424eb053e0a14e5ba
Jessy Govart <jessie.govart@cor.europa.eu>
2022-04-28 11:11:35 +0200

    toggle files cleaned

--- src/all.js
+++ src/all.js
@@ -3,5 +3,4 @@ import './components/expandinglist/expandinglist.js';
 // import './components/gallery/gallery.js';
 import './components/toc/toc.js';
 import './components/toggle/toggle.js';
-import './components/toggle/togglev2.js';
 import './components/twitter/twitter.js';
