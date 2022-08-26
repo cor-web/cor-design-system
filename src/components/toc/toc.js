03c03c00ea754056c42cb54f40ccbb3002115034
Jessie Govart <jessie.govart@cor.europa.eu>
2022-08-26 08:14:35 +0200

    delegation grid correction

--- src/components/toc/toc.js
+++ src/components/toc/toc.js
@@ -60,7 +60,77 @@ class TableOfContent extends HTMLElement {
     this.sections = this.getSections();
 
     this.root.innerHTML = `
-      <style>
+    <style>
+        ul {  
+          margin: var(--space-3xs) 0 0 0;
+          padding: 0;
+          list-style: none;
+        }
+        
+        ul > li {
+          border-left: solid 2px var(--oc-gray-3);
+          padding: var(--space-4xs) var(--space-2xs);
+          line-height: initial;
+        }
+        
+        /*
+        ul > li.active {
+          background-color: rgba(var(--oc-blue-8-rgb), .1);
+          border-left: solid 2px var(--oc-blue-8);
+        }
+        */
+        
+        h2 {
+          color: var(--oc-gray-7);
+          font-size: var(--step--3) !important;
+          font-weight: normal;
+          letter-spacing: 1.5px;
+          text-transform: uppercase;
+          margin: 0 0 var(--space--4xs) 0;
+        }
+        
+        a {
+          font-size: var(--step--4);
+          text-decoration: none;
+          margin-top: var(--space-4xs, 1rem);
+          outline: 2px solid transparent;
+          outline-offset: 5px;
+          transition: outline-offset 0.2s linear;
+        }
+        
+        a:link,
+        a:focus,
+        a:visited,
+        a:active {
+          color: var(--link-color);
+        }
+
+        a:hover {
+          text-decoration: none;
+          color: var(--txt-color);
+        }
+
+        /*
+        a:active {
+          color: #fff;
+          background-color: #024;
+        }
+        */
+
+        a:focus {
+          outline-color: currentColor;
+          outline-offset: 2px;
+        }
+      
+        :host {
+          display: block;
+        }
+        .readingSection {
+          
+        }
+      </style>
+      <!--  
+    <style>
       
       ul {
         display: grid;
@@ -117,6 +187,7 @@ class TableOfContent extends HTMLElement {
         display: block;
       }
       </style>
+      -->
       <nav class="cor-toc cor-toc--sticky" aria-labelledby="sections-heading">
         <h2 id="sections-heading"><slot name="toc-title">Contents</slot></h2>
         <ul>
