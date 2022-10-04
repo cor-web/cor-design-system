a56deff148f15064f3019f48dbfefbc12f397cfc
Jessie Govart <jessie.govart@cor.europa.eu>
2022-10-04 15:17:12 +0200

    scroll to top showButtopn when scroll

--- src/components/scrolltotop/scrolltotop.js
+++ src/components/scrolltotop/scrolltotop.js
@@ -1,9 +1,11 @@
+const rootElement = document.documentElement;
 const scrollToTopBtn = document.getElementById("scrollToTopBtn")
 
+/*
 const target = document.querySelector(".mainContent");
 
 function callback(entries, observer) {
-  entries.forEach( entry => {
+  entries.forEach(entry => {
     if (entry.isIntersecting) {
       // Show button
       scrollToTopBtn.classList.add('showBtn')
@@ -17,3 +19,18 @@ function callback(entries, observer) {
 let observer = new IntersectionObserver(callback);
 
 observer.observe(target);
+*/
+
+function handleScroll() {
+  // Do something on scroll
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.8) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
+  }
+}
+
+document.addEventListener('scroll', handleScroll);
